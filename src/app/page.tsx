010fb8e2a6530938e993c18aa59fc351c8c16763
andresc4f <caangogi@gmail.com>
2024-01-25 01:36:06 +0100

    add images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -6,9 +6,12 @@ import AntiWrinkle from '@/components/home-components/AntiWrinkle'
 import CentralLogo from '@/components/home-components/CentralLogo'
 import HerbalGreenPeel from '@/components/home-components/HerbalGreenPeel'
 import IvDrip from '@/components/home-components/IvDrip'
+import Header from '@/components/headers/header'
 
 export default function Home() {
   return (
+    <>
+      <Header />
     <main className={styles.main}>
       <div className={styles.main_container}>
 
@@ -28,5 +31,6 @@ export default function Home() {
         <HerbalGreenPeel />
       </div>
     </main>
+    </>
   )
 }
