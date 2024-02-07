8c055a53742a3f6c974f44b7e9c6b5d78316c513
andresc4f <99268322+caangogi@users.noreply.github.com>
2024-02-07 12:30:41 +0100

    add framer motion

--- src/components/home-components/AnimateCard.tsx
+++ src/components/home-components/AnimateCard.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import styles from '@/styles/home-components/AnimateCard.module.scss';
 import AnimatedLink from '@/components/links/animatedLink'
-import Link from 'next/link';
 
 interface AnimateCardProps {
   id: number;
--- src/components/pages/SurgicalCare.tsx
+++ src/components/pages/SurgicalCare.tsx
@@ -1,49 +1,68 @@
 import React from 'react';
 import styles from '@/styles/pages/surgical-care-page.module.scss';
-import {AnimatePresence, motion } from 'framer-motion'; 
+import { motion } from 'framer-motion';
+import { useInView } from 'react-intersection-observer';
+
+interface SectionProps {
+  children: React.ReactNode; 
+  className?: string; 
+}
+
+const Section: React.FC<SectionProps> = ({ children, className }) => {
+
+  const { ref, inView } = useInView({
+    triggerOnce: false,
+    threshold: 0.1,
+  });
+
+  return (
+    <motion.div
+      ref={ref}
+      className={className}
+      initial={{ opacity: 0, y: -100 }} 
+      animate={{
+        opacity: inView ? 1 : 0,
+        y: inView ? '-10vh' : '0'
+      }}
+      transition={{ duration: 0.5, delay: 0.2  }}
+    >
+      {children}
+    </motion.div>
+  );
+};
 
 export default function SurgicalCareDetails() {
   return (
     <div className={styles.surgical_care_page}>
-        <section className={`${styles.section} ${styles.section1}`}>
-            <AnimatePresence>
-              <motion.div 
-                className={`${styles.section1_content}`}
-                initial={{ opacity: 0, y: 50 }}
-                animate={{ opacity: 1, y: 0 }}
-                exit={{ opacity: 0, y: 50 }}
-                transition={{ duration: 0.75 }}
-              >
-                <div className={styles.text_container}>
-                   <h2 className={styles.title_page}>Dale el mejor cuidado a tu cuerpo después de la cirugía: Sesión Única de LDM para un Cuidado Postquirúrgico Superior</h2>
-                </div>
-              </motion.div>
-            </AnimatePresence>
-        </section>
+      <Section className={`${styles.section} ${styles.section1}`}>
+        <div className={styles.text_container}>
+          <h2 className={styles.title_page}>Dale el mejor cuidado a tu cuerpo después de la cirugía: Sesión Única de LDM para un Cuidado Postquirúrgico Superior</h2>
+        </div>
+      </Section>
 
-        <section className={`${styles.section} ${styles.section2}`}>
-          {/* Contenido de la sección 2 */}
-        </section>
+      <Section className={`${styles.section} ${styles.section2}`}>
+        <div className={styles.text_container}>
+          <h2 className={styles.title_page}>Dale el mejor cuidado a tu cuerpo después de la cirugía: Sesión Única de LDM para un Cuidado Postquirúrgico Superior</h2>
+        </div>
+      </Section>
 
-        <section className={`${styles.section} ${styles.section3}`}>
-          {/* Contenido de la sección 3 */}
-        </section>
+      <Section className={`${styles.section} ${styles.section3}`}>
+        <div className={styles.text_container}>
+          <h2 className={styles.title_page}>Dale el mejor cuidado a tu cuerpo después de la cirugía: Sesión Única de LDM para un Cuidado Postquirúrgico Superior</h2>
+        </div>
+      </Section>
 
-        <section className={`${styles.section} ${styles.section4}`}>
-          {/* Contenido de la sección 4 */}
-        </section>
+      <Section className={`${styles.section} ${styles.section4}`}>
+        <div className={styles.text_container}>
+          <h2 className={styles.title_page}>Dale el mejor cuidado a tu cuerpo después de la cirugía: Sesión Única de LDM para un Cuidado Postquirúrgico Superior</h2>
+        </div>
+      </Section>
 
-        <section className={`${styles.section} ${styles.section5}`}>
-          <div className={styles.content}>
-            {/* Contenido principal */}
-          </div>
-          <aside className={styles.aside}>
-            {/* Contenido secundario */}
-          </aside>
-          <footer className={styles.footer}>
-            {/* Pie de página */}
-          </footer>
-        </section>
+      <Section className={`${styles.section} ${styles.section5}`}>
+        <div className={styles.text_container}>
+          <h2 className={styles.title_page}>Dale el mejor cuidado a tu cuerpo después de la cirugía: Sesión Única de LDM para un Cuidado Postquirúrgico Superior</h2>
+        </div>
+      </Section>
     </div>
-  )
+  );
 }
